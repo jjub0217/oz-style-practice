349869a8a4928b31615e53989e1b1bc667e1ea67
jjub0217 <noonssub1004@gmail.com>
2025-06-01 17:18:12 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import "./App.scss";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
+
+import styled from "styled-components";
+import { flexMixin } from "./styled/styled";
+
+const StyleSection = styled.section`
+  ${flexMixin({
+    justify: "center",
+    align: "center",
+    wrap: "wrap",
+    gap: 20,
+  })}
+  padding: 20px 40px;
+`;
 
 function App() {
   return (
     <main>
       <Header />
-      <section>
+      <StyleSection>
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
-      </section>
+      </StyleSection>
     </main>
   );
 }
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,41 @@
+import styled from "styled-components";
+import { flexMixin, font, gray, tag_color } from "../styled/styled";
+
+const ContentContainer = styled.div`
+  ${flexMixin({
+    direction: "column",
+    align: "flex-start",
+    gap: 5,
+  })}
+  img {
+    width: 300px;
+    min-height: 225px;
+    margin-bottom: 3px;
+    border-radius: 10px;
+  }
+  span {
+    ${font({ fontSize: 12 })}
+    color: ${tag_color};
+    border: 1px solid ${tag_color};
+    padding: 4px 5px;
+    border-radius: 3px;
+  }
+  div {
+    ${font({ fontSize: 18, fontWeight: 600 })}
+  }
+  p {
+    ${font({ fontSize: 12 })}
+    color: ${gray};
+  }
+`;
+
 export default function Content({ content }) {
   return (
-    <div className="content-container">
+    <ContentContainer className="content-container">
       <img src={content.img} alt={content.title} />
       <span>모집중</span>
       <div>{content.title}</div>
       <p>{content.subtitle}</p>
-    </div>
+    </ContentContainer>
   );
 }
